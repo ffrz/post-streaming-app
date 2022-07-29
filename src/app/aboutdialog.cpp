ca0da8e5a078d0502351cee8d6706c5e15a02f9e
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2022-07-29 21:50:11 +0700

    update about dialog

--- src/app/aboutdialog.cpp
+++ src/app/aboutdialog.cpp
@@ -13,11 +13,11 @@ AboutDialog::AboutDialog(QWidget *parent) :
     setWindowTitle(QString("Tentang %1").arg(APP_DISPLAY_NAME));
 
     ui->infoLabel->setTextFormat(Qt::RichText);
-    ui->infoLabel->setText(QString(
-                                  "<h3>%1 %2</h3>"
-                                  "<p>Berbasis Qt %3</p>"
-                                  "<p>© 2021 Shift IT Solution</p>")
-                              .arg(APP_DISPLAY_NAME, APP_VERSION_STR, QT_VERSION_STR));
+    ui->infoLabel->setText(QString("<h3>%1 %2</h3>"
+                                   "<p>Berbasis Qt %3</p>"
+                                   "<p>Saran & Masukan: <a href=\"https://api.whatsapp.com/send?phone=6285224295073\">0853-1740-4760</a></p>"
+                                   "<p>© 2022 Shift IT Solution</p>")
+                           .arg(APP_DISPLAY_NAME, APP_VERSION_STR, QT_VERSION_STR));
 
     connect(ui->closeButton, SIGNAL(clicked()), SLOT(accept()));
     adjustSize();
